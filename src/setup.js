6cbda8c5091da22bada47ecb26fa7b271507759d
Arman Mahtabfar <arman.mahtabfar@gmail.com>
2021-07-28 10:57:37 -0400

    updated docker and added setup.js

---
+++ src/setup.js
@@ -0,0 +1,11 @@
+const WAYSCRIPT_ORIGIN = process.env.WAYSCRIPT_ORIGIN || "https://api.wayscript.com";
+
+const PROCESSES_ROUTE = "processes";
+const LAIRS_ROUTE = "lairs";
+const WORKSPACES_ROUTE = "workspaces";
+
+const ROUTES = {
+    "lairs": {"detail": LAIRS_ROUTE + "/$id"},
+    "wokspaces": {"detail": WORKSPACES_ROUTE + "/$id"},
+    "processes": {"detail_expanded": PROCESSES_ROUTE + "/$id/detail"}
+}
