b1fa0752d134bede313bcdc673a63cede8d99c33
Jonathan Meier <jonathan@wayscript.com>
2021-09-27 12:32:02 -0400

    Add webhooks route

--- src/setup.js
+++ src/setup.js
@@ -2,12 +2,14 @@ const WAYSCRIPT_ORIGIN = process.env.WAYSCRIPT_ORIGIN || "https://api.wayscript.
 
 const PROCESSES_ROUTE = "processes";
 const LAIRS_ROUTE = "lairs";
+const WEBHOOKS_ROUTE = "webhooks";
 const WORKSPACES_ROUTE = "workspaces";
 
 const ROUTES = {
     lairs: {"detail": LAIRS_ROUTE + "/$id"},
+    processes: {"detail_expanded": PROCESSES_ROUTE + "/$id/detail"},
+    webhooks: {"http_trigger_response": WEBHOOKS_ROUTE + "/http-trigger/response/$id"},
     workspaces: {"detail": WORKSPACES_ROUTE + "/$id"},
-    processes: {"detail_expanded": PROCESSES_ROUTE + "/$id/detail"}
 }
 
 module.exports = {WAYSCRIPT_ORIGIN, ROUTES};
