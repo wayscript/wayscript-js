06e0593df238af763025df62a3b565879a9d707b
Jonathan Meier <jonathan@wayscript.com>
2021-10-04 08:43:05 -0400

    Properly declare variables

--- src/triggers/http_trigger.js
+++ src/triggers/http_trigger.js
@@ -1,20 +1,19 @@
-utils = require('../utils');
+const utils = require('../utils');
 
 
 function sendResponse(data, headers, status_code) {
-    client = new utils.WayScriptClient();
-    process_id = utils.getProcessUUID();
+    let client = new utils.WayScriptClient();
+    let process_id = utils.getProcessUUID();
     // headers cannot be null
-    headers = headers === null ? {} : headers
+    headers = headers === null ? {} : headers;
 
-    payload = {
+    let payload = {
         "data": data,
         "headers": headers,
         "status_code": status_code
     };
-    payload_json = JSON.stringify(payload);
-    console.log(payload_json)
-    let [responseStatus, responseJson] = client.postWebhookHttpTrigger(process_id, payload_json);
+    let payload_json = JSON.stringify(payload);
+    let [, responseJson] = client.postWebhookHttpTrigger(process_id, payload_json);
     
     return responseJson;
 }
