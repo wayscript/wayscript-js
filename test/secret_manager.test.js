d57b9a1717a58aa83795668ff82fa3337f1ee405
Hannah Greene <hannah@wayscript.com>
2022-11-21 13:50:46 -0500

    jest

--- test/secret_manager.test.js
+++ test/secret_manager.test.js
@@ -21,7 +21,7 @@ test('Set Secret returns passed error message on 404', () => {
     try {
         secret_manager.setSecret("test_key", "test_value")
     } catch (err) {
-        expect(err).toEqual(result)
+        expect(err).toEqual(JSON.stringify(result))
         return  // short circuit
     }
     expect(true).toEqual(false)  // fail if no error thrown
