7d6823e9d22d6c53bb8fc6a174a933a6557eb174
Hannah Greene <hannah@wayscript.com>
2022-11-21 13:14:47 -0500

    jest

--- test/secret_manager.test.js
+++ test/secret_manager.test.js
@@ -10,8 +10,8 @@ test('Set Secret returns no info on success', () => {
     let payload = {};
 
     jest.spyOn(utils.WayScriptClient.prototype, "setLairSecret").mockImplementationOnce(() => [200, payload]);
-    let test_fn = () => { JSON.parse(secret_manager.setSecret("test_key", "test_value")) }
-    expect(test_fn()).toStrictEqual(payload);
+    let test_fn = () => { secret_manager.setSecret("test_key", "test_value") }
+    expect(test_fn()).toEqual(payload);
 });
 
 test('Set Secret returns passed error message on 404', () => {
