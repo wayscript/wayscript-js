e29daf65041add5f52b95110cbb202b6a1386b8d
Hannah Greene <hannah@wayscript.com>
2022-11-21 12:59:55 -0500

    jest

--- test/secret_manager.test.js
+++ test/secret_manager.test.js
@@ -7,17 +7,19 @@ jest.mock("../src/context");
 context.getProcess.mockImplementation(() => `{"lair_id": "fake_lair_id"}`);
 
 test('Set Secret returns no info on success', () => {
-    let payload = `{ }`;
+    let payload = {};
 
     jest.spyOn(utils.WayScriptClient.prototype, "setLairSecret").mockImplementationOnce(() => [200, payload]);
-    expect(secret_manager.setSecret("test_key", "test_value")).toStrictEqual(JSON.parse(payload));
+    expect(JSON.parse(() => { secret_manager.setSecret("test_key", "test_value") })).toStrictEqual(payload);
 });
 
 test('Set Secret returns passed error message on 404', () => {
     let error = {error: "wayscript backend error message"};
+    let result = {status: "404", json: error}
     
     jest.spyOn(utils.WayScriptClient.prototype, "setLairSecret").mockImplementationOnce(() => [404, error]);
-    expect(() => { secret_manager.setSecret("test_key", "test_value") }).toThrow("wayscript backend error message");
+    expect(JSON.parse(() => { secret_manager.setSecret("test_key", "test_value") })).toStrictEqual(result)
+    //expect(() => { secret_manager.setSecret("test_key", "test_value") }).toThrow("wayscript backend error message");
 });
 
 test('Set Secret returns unique error message on 403', () => {
