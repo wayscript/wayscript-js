3cc75fc341329e42df589cf9f9048b3635d6d183
Hannah Greene <hannah@wayscript.com>
2022-11-21 13:55:14 -0500

    jest

--- test/secret_manager.test.js
+++ test/secret_manager.test.js
@@ -15,21 +15,27 @@ test('Set Secret returns no info on success', () => {
 
 test('Set Secret returns passed error message on 404', () => {
     let error = {error: "wayscript backend error message"};
-    let result = {status: "404", json: error}
+    let result = {status: "404", json: error};
     
     jest.spyOn(utils.WayScriptClient.prototype, "setLairSecret").mockImplementationOnce(() => [404, error]);
     try {
-        secret_manager.setSecret("test_key", "test_value")
+        secret_manager.setSecret("test_key", "test_value");
     } catch (err) {
-        expect(err).toEqual(JSON.stringify(result))
-        return  // short circuit
+        expect(err).toEqual(JSON.stringify(result));
+        return;  // short circuit
     }
-    expect(true).toEqual(false)  // fail if no error thrown
+    expect(true).toEqual(false);  // fail if no error thrown
 });
 
 test('Set Secret returns unique error message on 403', () => {
     let result = {status: "403", json: {error: "user is not authorized to modify lair"}};
 
-    jest.spyOn(utils.WayScriptClient.prototype, "setLairSecret").mockImplementationOnce(() => [403, {}])
-    expect(secret_manager.setSecret("test_key", "test_value")).toThrow(result);
+    jest.spyOn(utils.WayScriptClient.prototype, "setLairSecret").mockImplementationOnce(() => [403, {}]);
+    try {
+        secret_manager.setSecret("test_key", "test_value");
+    } catch(err) {
+        expect(err).toEqual(JSON.stringify(result));
+        return;  // short circuit
+    }
+    expect(true).toEqual(false);  // fail if no error thrown
 });
