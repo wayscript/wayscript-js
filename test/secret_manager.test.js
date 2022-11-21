540b1eda7a9ac2307cf3d90b5f948c8a7bad5835
Hannah Greene <hannah@wayscript.com>
2022-11-21 12:42:43 -0500

    jest

--- test/secret_manager.test.js
+++ test/secret_manager.test.js
@@ -10,23 +10,20 @@ context.getProcess.mockImplementation(() => `{"lair_id": "fake_lair_id"}`)
 test('Set Secret returns no info on success', () => {
     let payload = {};
 
-    let client = new utils.WayScriptClient()
-    client.setLairSecret.mockImplementationOnce(() => [200, payload])
+    utils.WayScriptClient.setLairSecret.mockImplementationOnce(() => [200, payload])
     expect(secret_manager.setSecret("test_key", "test_value")).toStrictEqual(JSON.parse(payload));
 });
 
 test('Set Secret returns passed error message on 404', () => {
     let error = {error: "wayscript backend error message"};
     
-    let client = new utils.WayScriptClient()
-    client.setLairSecret.mockImplementationOnce(() => [404, error])
+    utils.WayScriptClient.setLairSecret.mockImplementationOnce(() => [404, error])
     expect(() => { secret_manager.setSecret("test_key", "test_value") }).toThrow("wayscript backend error message");
 });
 
 test('Set Secret returns unique error message on 403', () => {
     //let error = {error: "user is not authorized to modify lair"};
 
-    let client = new utils.WayScriptClient()
-    client.setLairSecret.mockImplementationOnce(() => [403, {}])
+    utils.WayScriptClient.setLairSecret.mockImplementationOnce(() => [403, {}])
     expect(secret_manager.setSecret("test_key", "test_value")).toStrictEqual(JSON.parse(`{ "Success" : "False", "Status" : "403"}`));
 });
